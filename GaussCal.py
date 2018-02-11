eefe0c8e6fa8de46eac3a48444c0049a59ee311c
DESKTOP-EQ7OUGA\mmmmmmmm <sitaziki@gmail.com>
2018-02-12 00:22:57 +0900

    書きかけのコード

---
+++ GaussCal.py
@@ -0,0 +1,11 @@
+#coding:utf8
+"""
+ガウスの数式で計算する
+n:1以上の正の数
+"""
+
+def gausscal(n):
+    sum = 0
+    sum = ((n + 1) * n ) /2
+    return sum
+
---
+++ main.py
@@ -0,0 +1,7 @@
+#coding:utf8
+#import GaussCal
+print("1～nまでの和で求めたい範囲を指定してください。")
+n = raw_input()
+int(n)
+print "1から%dまでのすべての和は、%" % n
+
