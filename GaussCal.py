210319c64151a4d0b72d782cb87a6d45f7504a99
mnara1002 <sitaziki@gmail.com>
2018-02-12 14:12:04 +0900

    コメントとprint内容を修正しました。これでfix完了です。

--- GaussCal.py
+++ GaussCal.py
@@ -1,11 +1,10 @@
 #coding:utf8
 """
-ガウスの数式で計算する
+ガウス少年の数式で計算する
 n:1以上の正の数
 """
 
 def gausscal(n):
-    sum = 0
+    sum = 0 #答えを格納する変数sumを初期化する
     sum = ((n + 1) * n ) /2
-    return sum
-
+    return sum #計算結果を返却する
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 #coding:utf8
 from GaussCal import gausscal
 from __builtin__ import int
-print("1～nまでの和で求めたい範囲を指定してください。")
+print("何日後の貯金額が知りたいですか？1以上の正の数で入力してください")
 input_num = raw_input()
-
-sum = gausscal(int(input_num))
-
-print "1から%sまでのすべての和は、%dです" % (input_num,sum)
-
+sum = gausscal(int(input_num)) #gausscal関数に入力した値をint型で渡し、戻り値を変数sumに代入する
+print "%s日後の貯金額は、%d円です" % (input_num,sum) #結果を表示する。変数input_numはstr型であるため、%sで出力させている
